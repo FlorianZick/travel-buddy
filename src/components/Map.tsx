b94bb9334d4d6022f9dd531f36f8c8e4bd3cf32f
Mark Oelmaier <oelmaier.mark@gmail.com>
2023-01-05 18:48:34 +0100

    Fix routing machine and export route

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,101 +8,143 @@ import { ConfigContext } from "./ConfigContext";
 import { useContext } from "react";
 import { Language } from "./ConfigContext/types";
 import { WikiApiDataModel } from "../models/wikiApiDataModel";
+import RoutingMachine, {
+    setCurrentPosition,
+    setDestinationPosition,
+} from "./RoutingMachine";
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
 // interface for props
 interface Props {
-  onLocationChange: React.Dispatch<React.SetStateAction<WikiApiDataModel[] | null>>;
-  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    onLocationChange: React.Dispatch<
+        React.SetStateAction<WikiApiDataModel[] | null>
+    >;
+    setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    children?: React.ReactNode;
 }
 
 function LocationMarker(props: Props) {
-  const { configs } = useContext(ConfigContext);
+    const { configs } = useContext(ConfigContext);
 
-  const [position, setPosition] = React.useState<L.LatLng>();
-  const [bbox, setBbox] = React.useState<string[]>([]);
-  const map = useMap();
+    const [position, setPosition] = React.useState<L.LatLng>();
+    const [bbox, setBbox] = React.useState<string[]>([]);
+    const map = useMap();
 
-  React.useEffect(() => {
-    // On every language change, remove the old event listeners and add new ones
-    // Callbacks needs to be changed, to make sure the new language is used in the fetchWikiApi call
-    map.locate().removeEventListener("locationfound");
+    React.useEffect(() => {
+        // On every language change, remove the old event listeners and add new ones
+        // Callbacks needs to be changed, to make sure the new language is used in the fetchWikiApi call
+        map.locate().removeEventListener("locationfound");
 
-    // Only adds a callback to the event listener and does not replace it
-    map.locate().on("locationfound", mapLocateCallback);
+        // Only adds a callback to the event listener and does not replace it
+        map.locate().on("locationfound", mapLocateCallback);
 
-    map.removeEventListener("click");
-    map.on("click", mapClickCallback);
-  }, [configs.language]);
+        map.removeEventListener("click");
+        map.on("click", mapClickCallback);
+    }, [configs.language]);
 
-  const mapLocateCallback = React.useCallback(
-    (e: any) => {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-      const radius = e.accuracy;
-      const circle = L.circle(e.latlng, radius);
-      circle.addTo(map);
-      setBbox(e.bounds.toBBoxString().split(","));
-      loadLocationData(e.latlng, props.onLocationChange, configs.language).then(() => {props.setModalOpen(true)});
-    },
-    [configs.language]
-  );
+    const mapLocateCallback = React.useCallback(
+        (e: any) => {
+            setPosition(e.latlng);
+            setCurrentPosition(e.latlng);
+            map.flyTo(e.latlng, map.getZoom());
+            const radius = e.accuracy;
+            const circle = L.circle(e.latlng, radius);
+            circle.addTo(map);
+            setBbox(e.bounds.toBBoxString().split(","));
+            loadLocationData(
+                e.latlng,
+                props.onLocationChange,
+                configs.language
+            ).then(() => {
+                props.setModalOpen(true);
+            });
+        },
+        [configs.language]
+    );
 
-  const mapClickCallback = React.useCallback(
-    (e: any) => {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-      loadLocationData(e.latlng, props.onLocationChange, configs.language).then(() => {props.setModalOpen(true)});
-    },
-    [configs.language]
-  );
+    const mapClickCallback = React.useCallback(
+        (e: any) => {
+            setPosition(e.latlng);
+            setDestinationPosition(e.latlng);
+            map.flyTo(e.latlng, map.getZoom());
+            loadLocationData(
+                e.latlng,
+                props.onLocationChange,
+                configs.language
+            ).then(() => {
+                props.setModalOpen(true);
+            });
+        },
+        [configs.language]
+    );
 
-  async function loadLocationData(
-    location: LatLng,
-    setWikiData: any,
-    lang: Language
-  ) {
-    const locationData = await reverseGeoEncoding(location.lat, location.lng);
+    async function loadLocationData(
+        location: LatLng,
+        setWikiData: any,
+        lang: Language
+    ) {
+        const locationData = await reverseGeoEncoding(
+            location.lat,
+            location.lng
+        );
 
-    const wikiData = await fetchWikiData(locationData.city, lang);
+        const wikiData = await fetchWikiData(locationData.city, lang);
 
-    setWikiData(wikiData);
-  }
+        setWikiData(wikiData);
+    }
 
-  return position === undefined ? null : (
-    <Marker position={position}>
-      <Popup>
-        Mein Standort
-        <br />
-        (Blauer Kreis = Genauigkeit
-        <br /> des Standorts)
-      </Popup>
-    </Marker>
-  );
+    return position === undefined ? null : (
+        <Marker
+            position={position}
+            eventHandlers={{
+                click: (e) => {
+                    props.setModalOpen(true);
+                },
+            }}
+        >
+            {/* <Popup>
+                Mein Standort
+                <br />
+                (Blauer Kreis = Genauigkeit
+                <br /> des Standorts)
+            </Popup> */}
+        </Marker>
+    );
 }
 
-function Map(props: any) {
-  return (
-    <MapContainer
-      center={{ lat: 51.166, lng: 10.452 }}
-      zoom={13}
-      style={{ width: "100%", height: "100vh" }}
-      dragging={true}
-      zoomControl={false}
-    >
-      <TileLayer
-        attribution=""
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {props.children}
-      <LocationMarker onLocationChange={props.onLocationChange} setModalOpen={props.setModalOpen}/>
-    </MapContainer>
-  );
-}
+const Map: React.FC<Props> = ({
+    onLocationChange,
+    setModalOpen,
+    children,
+}): React.ReactElement => {
+    return (
+        <MapContainer
+            center={{ lat: 51.166, lng: 10.452 }}
+            zoom={13}
+            style={{ width: "100%", height: "100vh" }}
+            dragging={true}
+            zoomControl={false}
+        >
+            <TileLayer
+                attribution=""
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {children}
+            <LocationMarker
+                onLocationChange={onLocationChange}
+                setModalOpen={setModalOpen}
+            />
+            <RoutingMachine
+                pos1={{ lat: 50, lng: 10 }}
+                pos2={{ lat: 50.3, lng: 10.3 }}
+            />
+        </MapContainer>
+    );
+};
 
 export default Map;
--- src/components/SheetModal/header.tsx
+++ src/components/SheetModal/header.tsx
@@ -1,42 +1,49 @@
 import { ReactElement } from "react";
 import {
-  IonFab,
-  IonFabButton,
-  IonGrid,
-  IonIcon,
-  IonItem,
-  IonRow,
-  IonText,
+    IonFab,
+    IonFabButton,
+    IonGrid,
+    IonIcon,
+    IonItem,
+    IonRow,
+    IonText,
 } from "@ionic/react";
 import { navigateCircleOutline } from "ionicons/icons";
+import { exportRoute } from "../RoutingMachine";
+import { useContext } from "react";
+import { ConfigContext } from "../ConfigContext";
 
 // interface for props
 interface Props {
-  title: string | undefined;
+    title: string | undefined;
 }
 
 const Header: React.FC<Props> = ({ title }): ReactElement => {
-  return (
-    <IonItem>
-      <IonGrid style={{ marginBottom: "4rem" }}>
-        <IonRow
-          className="ion-justify-content-center ion-align-items-center"
-          style={{ marginBottom: "0.5rem" }}
-        >
-          <IonText style={{ fontSize: "1.3rem", fontWeight: 450 }}>
-            {title}
-          </IonText>
-        </IonRow>
-        <IonRow style={{ justifyContent: "center" }}>
-          <IonFab>
-            <IonFabButton>
-              <IonIcon icon={navigateCircleOutline}></IonIcon>
-            </IonFabButton>
-          </IonFab>
-        </IonRow>
-      </IonGrid>
-    </IonItem>
-  );
+    const { configs, setConfigs } = useContext(ConfigContext);
+    function exportRouteToNavigatorApp() {
+        exportRoute(configs.navigator);
+    }
+    return (
+        <IonItem>
+            <IonGrid style={{ marginBottom: "4rem" }}>
+                <IonRow
+                    className="ion-justify-content-center ion-align-items-center"
+                    style={{ marginBottom: "0.5rem" }}
+                >
+                    <IonText style={{ fontSize: "1.3rem", fontWeight: 450 }}>
+                        {title}
+                    </IonText>
+                </IonRow>
+                <IonRow style={{ justifyContent: "center" }}>
+                    <IonFab>
+                        <IonFabButton onClick={exportRouteToNavigatorApp}>
+                            <IonIcon icon={navigateCircleOutline}></IonIcon>
+                        </IonFabButton>
+                    </IonFab>
+                </IonRow>
+            </IonGrid>
+        </IonItem>
+    );
 };
 
 export default Header;
