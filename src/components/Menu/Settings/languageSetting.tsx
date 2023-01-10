385909fabf7f218318f5d69e675e43a169d649ed
Mark Oelmaier <oelmaier.mark@gmail.com>
2023-01-10 09:48:40 +0100

    Fix bug: strings broken after navigator change

--- src/components/Menu/Settings/languageSetting.tsx
+++ src/components/Menu/Settings/languageSetting.tsx
@@ -1,19 +1,24 @@
-import { ReactElement, useContext } from "react";
+import React, { ReactElement, useContext } from "react";
 import { IonItem, IonLabel, IonSelect, IonSelectOption } from "@ionic/react";
 
 import { Language } from "./../../ConfigContext/types";
 import { ConfigContext } from "./../../ConfigContext/ConfigContext";
 
 import { useTranslation } from "react-i18next";
+import L from "leaflet";
 
 const LanguageSetting: React.FC = (): ReactElement => {
     const { t, i18n } = useTranslation();
     const { configs, setConfigs } = useContext(ConfigContext);
-
+    React.useEffect(() => {
+        const divRef = document.getElementById("languageIonSelect")!;
+        L.DomEvent.disableClickPropagation(divRef);
+    });
     return (
         <IonItem style={{ margin: "0 20px" }}>
             <IonLabel>{t("settings.language")}</IonLabel>
             <IonSelect
+                id="languageIonSelect"
                 selectedText={configs.language}
                 onIonChange={(e) => {
                     configs.language = e.detail.value;
--- src/components/Menu/Settings/themeSetting.tsx
+++ src/components/Menu/Settings/themeSetting.tsx
@@ -1,23 +1,27 @@
-import { ReactElement, useContext } from "react";
+import React, { ReactElement, useContext } from "react";
 import { IonItem, IonLabel, IonSelect, IonSelectOption } from "@ionic/react";
 
 import { Theme } from "../../ConfigContext/types";
 import { ConfigContext } from "../../ConfigContext/ConfigContext";
 
 import { useTranslation } from "react-i18next";
+import L from "leaflet";
 
 const DarkModeSetting: React.FC = (): ReactElement => {
     const { t } = useTranslation();
     const { configs, setConfigs } = useContext(ConfigContext);
-
+    React.useEffect(() => {
+        const divRef = document.getElementById("themeIonSelect")!;
+        L.DomEvent.disableClickPropagation(divRef);
+    });
     return (
         <IonItem style={{ margin: "0 20px" }}>
             <IonLabel>{t("settings.theme")}</IonLabel>
             <IonSelect
+                id="themeIonSelect"
                 selectedText={configs.theme}
                 onIonChange={(e) => {
                     const theme = e.detail.value;
-
                     if (theme === Theme.LIGHT) {
                         document.body.classList.toggle("dark", false);
                     } else if (theme === Theme.DARK) {
@@ -31,7 +35,6 @@ const DarkModeSetting: React.FC = (): ReactElement => {
                             prefersDark.matches
                         );
                     }
-
                     configs.theme = e.detail.value;
                     setConfigs({ ...configs });
                 }}
