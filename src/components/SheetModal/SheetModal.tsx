e72a54aab07c6a3b6626e68ef68fa04be7f5cca0
Mark Oelmaier <oelmaier.mark@gmail.com>
2023-01-11 19:30:15 +0100

    Fix bug: Sheet modal scrollen auf Handy

--- src/components/SheetModal/SheetModal.tsx
+++ src/components/SheetModal/SheetModal.tsx
@@ -75,7 +75,7 @@ const SheetModal: React.FC<Props> = ({
                     setModalOpen(false);
                 }}
                 initialBreakpoint={0.5}
-                breakpoints={[0, 0.15, 0.5, 0.9]}
+                breakpoints={[0, 0.15, 0.5, 1.0]}
             >
                 <IonContent className="ion-padding">
                     <IonList>
