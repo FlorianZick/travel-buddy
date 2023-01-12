8d79f1c11583f949d3e82c56a51a7f88d4c562b7
Maximilian <email@example.com>
2023-01-12 19:07:22 +0100

    Added Searchbar

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,16 +1,44 @@
 import React, { ReactElement, useState } from "react";
-import Searchbar from "./Searchbar";
 import Settings from "./Settings/settings";
 import MenuButton from "./menuButton";
+import { useMap } from "react-leaflet";
+
+import {GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch'; 
+
+import Searchbar from "./Searchbar";
+import "./leaflet-geosearch.css"
 
 import "./menu.css";
 
-const Menu: React.FC = (): ReactElement => {
+interface Props {
+    apiKey?: string;
+}
+
+const SearchField = ({ apiKey } : {apiKey:any}) => {
+    const provider = new OpenStreetMapProvider({
+      params: {
+        access_token: apiKey,
+      },
+    });
+  
+    // @ts-ignore
+    const searchControl = new GeoSearchControl({
+      provider: provider,
+      style: 'bar',
+    });
+    
+    const map = useMap();
+    React.useEffect(() => {map.addControl(searchControl);}, [])
+  
+    return null;
+  };   
+
+const Menu: React.FC<Props> = ({apiKey}): ReactElement => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
         <div>
-            <Searchbar />
+            <SearchField apiKey={apiKey} />
             <MenuButton setIsOpen={setIsOpen} />
             <Settings isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
