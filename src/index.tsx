ccf00122b3c2f53183c44e5e3b36e8b5442e06f7
Mark Oelmaier <oelmaier.mark@gmail.com>
2023-01-22 14:10:31 +0100

    Default Sprache fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import reportWebVitals from "./reportWebVitals";
 import { setupIonicReact } from "@ionic/react";
 import ConfigsProvider from "./components/ConfigContext/ConfigContext";
 import {
-  Config,
-  DeviceType,
-  Language,
-  NavigatorApp,
-  Theme,
+    Config,
+    DeviceType,
+    Language,
+    NavigatorApp,
+    Theme,
 } from "./components/ConfigContext/types";
 import InformationsProvider from "./components/InformationContext/InformationContext";
 
@@ -17,8 +17,8 @@ import InformationsProvider from "./components/InformationContext/InformationCon
  * Enum for the component mode
  */
 enum Mode {
-  IOS = "ios",
-  MD = "md",
+    IOS = "ios",
+    MD = "md",
 }
 
 /**
@@ -26,18 +26,18 @@ enum Mode {
  * @returns is apple device
  */
 function isAppleDevice(): boolean {
-  return (
-    [
-      "iPad Simulator",
-      "iPhone Simulator",
-      "iPod Simulator",
-      "iPad",
-      "iPhone",
-      "iPod",
-    ].includes(navigator.platform) ||
-    // iPad on iOS 13 detection
-    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-  );
+    return (
+        [
+            "iPad Simulator",
+            "iPhone Simulator",
+            "iPod Simulator",
+            "iPad",
+            "iPhone",
+            "iPod",
+        ].includes(navigator.platform) ||
+        // iPad on iOS 13 detection
+        (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    );
 }
 
 /**
@@ -45,18 +45,18 @@ function isAppleDevice(): boolean {
  * @returns device mode
  */
 function detectMode(): Mode {
-  if (isAppleDevice()) {
-    return Mode.IOS;
-  } else {
-    return Mode.MD;
-  }
+    if (isAppleDevice()) {
+        return Mode.IOS;
+    } else {
+        return Mode.MD;
+    }
 }
 
 /**
  * Set device mode
  */
 setupIonicReact({
-  mode: detectMode(),
+    mode: detectMode(),
 });
 
 // read config data from local storage
@@ -64,30 +64,30 @@ const configStorageData = localStorage.getItem("configs");
 
 // set config data from local storage, if no data is found, set default values
 const configData: Config = configStorageData
-  ? JSON.parse(configStorageData)
-  : {
-      theme: Theme.LIGHT,
-      device: DeviceType.MOBILE,
-      language: Language.EN,
-      navigator: NavigatorApp.GOOGLE_MAPS,
-    };
+    ? JSON.parse(configStorageData)
+    : {
+          theme: Theme.LIGHT,
+          device: DeviceType.MOBILE,
+          language: Language.DE,
+          navigator: NavigatorApp.GOOGLE_MAPS,
+      };
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
-  // React strict mode will render twice in development phase, comment out to prevent this behaviour
-  // <React.StrictMode>
-  <InformationsProvider
-    curPosInformationInfo={null}
-    isModalOpen={false}
-    locationInfo={null}
-    showCurPosInformation={false}
-  >
-    <ConfigsProvider configJson={configData}>
-      <App />
-    </ConfigsProvider>
-  </InformationsProvider>
-  // </React.StrictMode>
+    // React strict mode will render twice in development phase, comment out to prevent this behaviour
+    // <React.StrictMode>
+    <InformationsProvider
+        curPosInformationInfo={null}
+        isModalOpen={false}
+        locationInfo={null}
+        showCurPosInformation={false}
+    >
+        <ConfigsProvider configJson={configData}>
+            <App />
+        </ConfigsProvider>
+    </InformationsProvider>
+    // </React.StrictMode>
 );
 
 serviceWorkerRegistration.register();
